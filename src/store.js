b1db63399ae4034424b4083c6c5709cfeaa6c2b1
themiyakasun <themiyakasun.tb@gmail.com>
2023-10-11 01:52:38 +0530

    Add Students and save on localstorage

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
-import create from 'zustand';
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
-const useStore = create((set) => ({
+let store = (set) => ({
   students: ['kasun', 'themiya', 'nihal'],
   addStudent: (student) => {
     set((state) => ({ students: [...state.students, student] }));
   },
-}));
+});
+
+store = devtools(store);
+store = persist(store, { name: 'students' });
+
+const useStore = create(store);
 
 export default useStore;
